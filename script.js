97c41e6bac0d086d74546ed9b7fc944497d3e0b0
fedaaehab <109865057+fedaaehab@users.noreply.github.com>
2022-08-17 19:48:12 +0300

    1

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function BMI (weight ,height){
+let result=weight / (height * height);
+return result
 
+}
+console.log(BMI(10,10))
+
+function Status(bmi){
+if(bmi<18.5){
+ return "لديك نقص في الوزن"   
+}
+
+if(25>bmi>=18.5){
+ return "وزنك صحي"   
+}
+
+if(bmi>=25){
+ return "لديك زيادة في الوزن"   
+}
+}
+
+function calculate(){
+
+    let weight = document.getElementById('weight').value
+    let height = document.getElementById('height').value 
+    let bmi=BMI(weight ,height)
+    let desc=Status(bmi) 
+    let div=document.getElementById('result') 
+    div.innerText=bmi + " == " + desc 
+}
